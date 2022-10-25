cf3e718e367bacde3aeb21c2cc2b327171881755
baguswibi <baguswibisono06@gmail.com>
2022-10-25 10:45:45 +0700

    menghitung luas dan keliling lingkaran menggunakan phyton

---
+++ luasdankelilinglingkaran.py
@@ -0,0 +1,9 @@
+import math
+
+
+r = float(input("masukan jari jari : "))
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+print ("luas lingkaran \t\t= ",luas)
+print ("keliling lingkaran\t= ",keliling)
+
